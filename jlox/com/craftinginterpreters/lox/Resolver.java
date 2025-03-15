42f1f34757ee0b7ec6a814df9f1e59b969151c22
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2025-03-15 19:04:32 +0000

    Chapter 11 challenge 3: dont mark assignment as use

--- jlox/com/craftinginterpreters/lox/Resolver.java
+++ jlox/com/craftinginterpreters/lox/Resolver.java
@@ -14,7 +14,7 @@ import java.util.Stack;
  */
 public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   private final Interpreter interpreter;
-  private final Stack<Map<String, ScopeVariable>> scopes = new Stack<>();
+  private final Stack<Map<String, Variable>> scopes = new Stack<>();
   private FunctionType currentFunction = FunctionType.NONE;
 
   Resolver(Interpreter interpreter) {
@@ -23,12 +23,12 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   private enum FunctionType { NONE, FUNCTION }
 
-  private class ScopeVariable {
+  private class Variable {
     enum State { DECLARED, DEFINED, USED }
     private State state = State.DECLARED;
     private final Token name;
 
-    ScopeVariable(Token name) {
+    Variable(Token name) {
       this.name = name;
     }
 
@@ -125,7 +125,7 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   @Override
   public Void visitAssignExpr(Expr.Assign expr) {
     resolve(expr.value);
-    resolveLocal(expr, expr.name);;
+    resolveLocal(expr, expr.name, false);;
     return null;
   }
 
@@ -181,12 +181,12 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   public Void visitVariableExpr(Expr.Variable expr) {
     // Here we prohibit using a variable in it's own initializer by checking if it is only declared but not yet initialized
     if (!scopes.isEmpty() &&
-        scopes.peek().get(expr.name.lexeme) != null &&
+        scopes.peek().containsKey(expr.name.lexeme) &&
         scopes.peek().get(expr.name.lexeme).isDeclared()) {
       Lox.error(expr.name, "Can't read local variable in its own initializer");
     }
 
-    resolveLocal(expr, expr.name);
+    resolveLocal(expr, expr.name, true);
     return null;
   }
 
@@ -205,30 +205,30 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   }
 
   private void beginScope() {
-    scopes.push(new HashMap<String, ScopeVariable>());
+    scopes.push(new HashMap<String, Variable>());
   }
 
   private void endScope() {
+    Map<String, Variable> scope = scopes.pop();
+
     // We check for unused variables and report them as errors
-    for (Map.Entry<String, ScopeVariable> entry : scopes.peek().entrySet()) {
-      ScopeVariable scopeVariable = entry.getValue();
+    for (Map.Entry<String, Variable> entry : scope.entrySet()) {
+      Variable scopeVariable = entry.getValue();
       if (!scopeVariable.isUsed()) {
         Lox.error(scopeVariable.getName(), "Unused variable '" + entry.getKey() + "'.");
       }
     }
-
-    scopes.pop();
   }
 
   private void declare(Token name) {
     if (scopes.isEmpty()) return;
 
-    Map<String, ScopeVariable> scope = scopes.peek();
+    Map<String, Variable> scope = scopes.peek();
     if (scope.containsKey(name.lexeme)) {
       Lox.error(name, "Variable already exists");
     }
 
-    scope.put(name.lexeme, new ScopeVariable(name));
+    scope.put(name.lexeme, new Variable(name));
   }
 
   private void define(Token name) {
@@ -236,13 +236,16 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     scopes.peek().get(name.lexeme).setDefined();
   }
 
-  private void resolveLocal(Expr expr, Token name) {
+  private void resolveLocal(Expr expr, Token name, boolean isRead) {
     // We go down the stack and try to resolve variable in the nearest scope
     for (int i = scopes.size()-1; i >= 0; i--) {
       if (scopes.get(i).containsKey(name.lexeme)) {
         interpreter.resolve(expr, scopes.size()-1-i);
-        // We mark function as used upon resolution to report unused errors later
-        scopes.get(i).get(name.lexeme).setUsed();
+
+        // We mark function as used upon resolution to report unused errors later but only when it is beaing read
+        if (isRead) {
+          scopes.get(i).get(name.lexeme).setUsed();
+        }
         return;
       }
     }
